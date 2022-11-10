805187a06a5c8976311104cd9bcdacf462061d56
Shriman Vinayagamoorthy <shriman30@hotmail.com>
2022-11-09 19:21:29 -0500

    Did the contact us section
    Still need to make sure the form works on submit and the still need to add appropriate images to the upcoming events section

--- src/components/ContactUs/ContactUs.jsx
+++ src/components/ContactUs/ContactUs.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
-import './ContactUs.css'
+import React from "react";
+import "./ContactUs.css";
+
 const ContactUs = () => {
   return (
-    <div>ContactUs</div>
-  )
-}
+    <div className="contact-us-container" id="contactUs">
+      <div className="header">
+        <h1>Contact Us</h1>
+      </div>
+      <div className="content">
+        <div className="contact-info">
+          <div className="email">
+            <img src={require('./Contact us 1 (2).png')} />
+            <p className="email-text">info@d2c.com</p>
+          </div>
+          <div className="phone">
+              <img src={require('./Contact us 1.png')} width={'400px'}/>
+            <p className="phone-text">(514)-321-7654</p>
+          </div>
+        </div>
+        <div className="form-container">
+          <div className="form">
+            <form action="onSubmit">
+              <input type="text" placeholder="Name" /><br />
+              <input type="email" placeholder="Email" /><br />
+              <textarea className="message" name="Message" id="message" cols="30" rows="10"placeholder="Message"></textarea>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default ContactUs
+export default ContactUs;
